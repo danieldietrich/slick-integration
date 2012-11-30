8b2f777f6b0e309993b33ba8a48a4ff2131e041d
Daniel Dietrich <cafebab3@gmail.com>
2012-12-01 00:00:19 +0100

    run each test in a new in memory database

--- src/test/scala/scala/slick/integration/SlickAccessLayerSpec.scala
+++ src/test/scala/scala/slick/integration/SlickAccessLayerSpec.scala
@@ -15,7 +15,7 @@ case class Product(name: String, description: String, id: Option[Long] = None) e
 trait ProductComponent extends _Component { self: Profile =>
 
   import profile.simple._
-  
+
   object Products extends Mapper[Product]("product") {
 
     def name = column[String]("name")
@@ -24,18 +24,18 @@ trait ProductComponent extends _Component { self: Profile =>
     def * = name ~ description ~ id.? <> (Product, Product.unapply _)
 
     lazy val existsNameQuery = for {
-      name <- Parameters[String] 
+      name <- Parameters[String]
     } yield Products.where(_.name === name).map(_.id).exists
-    
+
     def existsName(name: String): Boolean = db.withSession { implicit s: Session =>
       existsNameQuery.first(name)
     }
-    
+
     lazy val findByNameAndDescriptionQuery = for {
       (name, description) <- Parameters[(String, String)]
       p <- Products if p.name === name && p.description === description
     } yield p
-    
+
     def findByNameAndDescription1(name: String, description: String): Option[Product] = db.withSession { implicit s: Session =>
       findByNameAndDescriptionQuery.firstOption(name, description)
     }
@@ -48,30 +48,46 @@ trait ProductComponent extends _Component { self: Profile =>
 
 }
 
-class DAL extends _DAL with ProductComponent with Profile {
+object DAL extends _DAL with ProductComponent with Profile {
 
   // trait Profile implementation
-  val profile = H2Driver 
-  def db = Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver")
+  val profile = H2Driver
+
+  private val database = Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver")
+  def db = database
 
   // _DAL.ddl implementation
   lazy val ddl: DDL = Products.ddl
-  
+
   db.withSession { implicit s: Session =>
     import profile.simple._
-  	ddl.create
+    ddl.create
   }
 
 }
 
+object TestHelper {
+  def running[T](block: => T): T = {
+    synchronized {
+      DAL.db.withSession { implicit session: Session =>
+        try {
+          DAL.create
+          block
+        } finally {
+          DAL.drop
+        }
+      }
+    }
+  }
+}
+
 class DataAccessLayerSpec extends Specification {
 
-  val DAL = new DAL()
-  
   import DAL._
-  
+  import TestHelper.running
+
   "Slick Integration" should {
-    "be testable with in-memory h2 driver" in {
+    "be testable with in-memory h2 driver" in running {
       db.withSession { s: Session =>
         s.metaData.getDatabaseProductName must equalTo("H2")
       }
@@ -79,55 +95,39 @@ class DataAccessLayerSpec extends Specification {
   }
 
   "Product model" should {
-    
-    "crud" in {
-      db.withSession { implicit s: Session =>
-        
-        create
-        
-        val product = Products.insert(Product("name", "description"))
-        product.id must not equalTo (None) // AutoInc id correct
-        product.id.map { id =>
-          
-          Products.findById(id) must equalTo(Some(Product("name", "description", Some(id)))) // product found
-          Products.existsName("name") must equalTo(true)
-          Products.existsName("new") must equalTo(false)
-          
-          Products.update(product.copy(name = "new")) must equalTo(Product("new", "description", Some(id)))
-          Products.existsName("new") must equalTo(true)
-          Products.existsName("name") must equalTo(false)
-          
-          Products.delete(id) must equalTo(true) 
-          Products.findById(id) must equalTo(None)
-          Products.existsName("new") must equalTo(false)
-          
-        }
-        
-        drop
-        
-        success
-      }
+
+    "crud" in running {
+
+      val product = Products.insert(Product("name", "description"))
+      product.id must not equalTo (None) // AutoInc id correct
+      val id = product.id.get
+
+      Products.findById(id) must equalTo(Some(Product("name", "description", Some(id)))) // product found
+      Products.existsName("name") must equalTo(true)
+      Products.existsName("new") must equalTo(false)
+
+      Products.update(product.copy(name = "new")) must equalTo(Product("new", "description", Some(id)))
+      Products.existsName("new") must equalTo(true)
+      Products.existsName("name") must equalTo(false)
+
+      Products.delete(id) must equalTo(true)
+      Products.findById(id) must equalTo(None)
+      Products.existsName("new") must equalTo(false)
+
     }
-    
-//    "query template with two parameters" in {
-//      db.withSession { implicit s: Session =>
-//        
-//        create
-//        
-//        val product = Products.insert(Product("name", "description"))
-//        
-//        Products.findByNameAndDescription1("name", "description") should equalTo(Some(product))
-//        Products.findByNameAndDescription1("xxx", "description") should equalTo(None)
-//        
-//        Products.findByNameAndDescription2("name", "description") should equalTo(Some(product))
-//        Products.findByNameAndDescription2("xxx", "description") should equalTo(None)
-//        
-//        drop
-//        
-//        success
-//      }
-//    }
-    
+
+    "query template with two parameters" in running {
+      
+      val product = Products.insert(Product("name", "description"))
+
+      Products.findByNameAndDescription1("name", "description") should equalTo(Some(product))
+      Products.findByNameAndDescription1("xxx", "description") should equalTo(None)
+
+      Products.findByNameAndDescription2("name", "description") should equalTo(Some(product))
+      Products.findByNameAndDescription2("xxx", "description") should equalTo(None)
+      
+    }
+
   }
-  
+
 }
